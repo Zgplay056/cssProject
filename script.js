60b10fd144836c8802a08c590beaf3b8f5ef1960
Zgplay056 <gabrielgomesplay056@gmail.com>
2024-10-01 21:24:27 -0300

    Terminei o darkmode

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
 function darkLight () {    
       document.body.classList.toggle('darkMode');
       document.body.classList.toggle('lightMode');
+
+      var alterar = document.getElementById("alterar");
+      
+      
+      
+      if (alterar.classList.contains("bg-light")) {
+        alterar.classList.remove("bg-light");
+        alterar.classList.add("dark_div");
+      } else {
+        alterar.classList.remove("dark_div");
+        alterar.classList.add("bg-light");
+      }
+
+        // Checkbox
+        const checkbox = document.getElementById('darkModeToggle');
+        const icon = document.getElementById('icon');
+
+        checkbox.addEventListener('change', function() {
+            if (this.checked && icon.classList.contains('bi-moon')) {
+                icon.classList.remove('bi-moon');
+                icon.classList.add('bi-sun');
+            } else {
+                icon.classList.remove('bi-sun');
+                icon.classList.add('bi-moon');
+            }
+        });
     }
-   
   // this is too simple and it works correctly, be careful at the css variables
 
+
+  //Thats the nav being actualized
 const alterar = document.getElementById('alterar');
 const home = document.getElementById('home');
 const foco = document.getElementById('foco');
@@ -19,8 +46,10 @@ foco.addEventListener('click', () => {
 });
 
   estudos.addEventListener('click', () => {
- 
-  fetch('Estudos.html')
-  .then(response => response.text())
-  .then(html => alterar.innerHTML = html);
+  alterar.innerHTML = '<h1>Estudos Aqui</h1><p>Conteúdo da página...</p>';
 });
+
+toDo.addEventListener('click', () => {
+  alterar.innerHTML = '<h1>A Fazeres</h1><p>Conteúdo da página...</p>';
+});
+
